64f8e5b90bdf298fd5ccd564a3e2bf832ff0791f
UWL-HA <uwl-ha@users.noreply.github.com>
2026-08-26 16:20:15 +0200

    Add guarded ACL setup for lock bindings

--- custom_components/uwb_matter/__init__.py
+++ custom_components/uwb_matter/__init__.py
@@ -31,7 +31,7 @@ from .const import (
 from .history import UwbHistoryStore
 
 CARD_URL = "/uwb_matter/uwb-approach-card.js"
-CARD_RESOURCE_URL = f"{CARD_URL}?v=0.17.0-beta.5"
+CARD_RESOURCE_URL = f"{CARD_URL}?v=0.17.0-beta.6"
 CONFIG_SCHEMA = cv.config_entry_only_config_schema(DOMAIN)
 
 
